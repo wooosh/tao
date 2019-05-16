7a717906cabd48bcc0094e11195ba185ed3d8e73
sfplayer247 <>
2019-05-16 21:06:22 +0000

    Upload code

---
+++ Makefile
@@ -0,0 +1,16 @@
+tao: main.c
+	gcc main.c -lmagic -g -o tao
+
+.PHONY: config
+config: example.conf
+	mkdir -p ~/.config
+	cp example.conf ~/.config/tao.conf
+
+.PHONY: install
+install: tao
+	cp tao /usr/local/bin/tao
+
+.PHONY: uninstall
+uninstall:
+	rm ~/.config/tao.conf
+	rm /usr/local/bin/tao
---
+++ main.c
@@ -0,0 +1,110 @@
+// Standard Libraries
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+// External Libraries
+#include <magic.h>
+
+typedef struct KVPair {
+  char* key;
+  char* val;
+  FILE* file;
+} KVPair;
+
+KVPair initParser(char* filename) {
+  return (KVPair){malloc(1), malloc(1), fopen(filename, "r")};
+};
+
+char* stripstr(char* str, int direction, char delimeter) {
+  size_t len = strlen(str);
+  int index = direction ? 0 : len - 1;
+  while (0 <= index && index <= len && str[index] == delimeter)
+    direction ? index++ : index--;
+  
+  if (!direction)
+    str[index + 1] = '\0';
+  else
+    memmove(str, str + index, len - index + 1);
+  return str;
+}
+
+void nextPair(KVPair *pair) {
+  char* line = NULL;
+  size_t len = 0;
+  // Find a line that isn't a comment or blank line until we hit the end of the file
+  while (line == NULL || line[0] == '#' || line[0] == '\n')
+    if (getline(&line, &len, pair -> file) == -1) {
+      pair -> file = NULL;
+      return;
+    }
+  len = strlen(line);
+  //  Remove newline from getline
+  if (line[len-1] == '\n')
+    line[len-1] = '\0';
+  
+  // If the line does not contain a "=" mark it as an invalid line
+  if (strchr(line, '=') == NULL) {
+    printf("Invalid Configuration:\n '%s'", line);
+    exit(EXIT_FAILURE);
+  }
+  
+  char* key = stripstr(strtok(line, "="), 0, ' ');
+  char* val = stripstr(strtok(NULL, ""), 1, ' ');
+  
+  //free(line);  
+  // Allocate more memory if the structure members are not large enough
+  if (sizeof(pair -> key) < strlen(key) + 1) 
+    pair -> key = realloc(pair -> key, strlen(key) + 1);
+  if (sizeof(pair -> val) < strlen(val) + 1)
+    pair -> val = realloc(pair -> val, strlen(val) + 1);
+  
+  strcpy(pair -> key, key);
+  strcpy(pair -> val, val);
+};
+
+int main(int argc, char **argv) {
+  // Make sure we get an argument
+  if (argc < 2) {
+    printf("Not enough arguments\n");
+    return EXIT_FAILURE;
+  }
+  const char* file = argv[1];
+  // Get the files mime type
+  struct magic_set *magic = magic_open(MAGIC_MIME_TYPE | MAGIC_CHECK);
+  magic_load(magic, NULL);
+  const char* mimetype = magic_file(magic, file);
+  
+  // Get the program associated with the mime type from the config file
+  char* configLocation = malloc(strlen(getenv("HOME")) + strlen("/.config/tao.conf") + 1);
+  configLocation[0] = '\0';
+  sprintf(configLocation, "%s/.config/tao.conf", getenv("HOME"));
+  KVPair config = initParser(configLocation);
+
+  // If the file is not found, throw an error and exit
+  if (config.file == NULL) {
+    printf("Cannot locate config file at '~/.config/tao.conf'\n");
+    magic_close(magic);
+    return EXIT_FAILURE;
+  }
+  
+  nextPair(&config); 
+  while (1) {
+    if (strcmp(config.key, mimetype) == 0) {
+      // Run the associated program with the filename as an argument
+      char* cmd = malloc(strlen(config.val) + strlen(file) + 2);
+      sprintf(cmd, "%s %s", config.val, file);
+      system(cmd);
+      magic_close(magic);
+      return EXIT_SUCCESS;
+    }
+    if (config.file == NULL) 
+      break;
+    nextPair(&config);
+  }
+  
+  printf("No program associated with mimetype: %s\n", mimetype);
+  // Cleanup and exit
+  magic_close(magic);
+  return EXIT_FAILURE;
+}
